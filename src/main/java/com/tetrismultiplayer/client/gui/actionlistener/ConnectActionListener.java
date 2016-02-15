e375c60981f0f49f59aa6f84a53d7bcc9b4eab64
MuskalaMarcin <marcin.muskala@gmail.com>
2016-02-15 18:13:23 +0100

    converting to intellij project

--- src/main/java/com/tetrismultiplayer/client/gui/actionlistener/ConnectActionListener.java
+++ src/main/java/com/tetrismultiplayer/client/gui/actionlistener/ConnectActionListener.java
@@ -73,7 +73,7 @@ public class ConnectActionListener implements ActionListener
 		}
 		catch (IOException e1)
 		{
-		    e1.printStackTrace();
+		    leftPanel.setStatusText("B³¹d");
 		}
 	    }
 	}
